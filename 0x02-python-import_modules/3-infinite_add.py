5cb7a9b67e389d2b94a14de7cc708a2454f0ff2f
Samuel Awe <Samuelawe842@gmail.com>
2022-06-03 02:16:51 +0100

    import modules commit

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,4 +1,5 @@
- __name__ == "__main__":
+#!/usr/bin/python3
+if __name__ == "__main__":
     from sys import argv
     res = 0
     for arg in argv[1:]:
--- 0x02-python-import_modules/README.md
+++ 0x02-python-import_modules/README.md
@@ -1,3 +1,1 @@
 # Import & Modules
-
-This project contains some tasks for learning about imports and modules in Python.
